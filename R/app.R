789c221a4f8882bf2b74eef1be5525a65be19836
Peter Winskill <p.winskill@imperial.ac.uk>
2023-02-08 10:10:56 +0000

    button test working

--- R/app.R
+++ R/app.R
@@ -7,11 +7,16 @@
 app <- function(){
 
   ui <- shiny::fluidPage(
-
-    )
+    shiny::numericInput("N", label = "N", value = 0),
+    shiny::textOutput("message"),
+    shiny::actionButton("button","Update message")
+  )
 
   server <- function(input, output, session) {
 
+    shiny::observeEvent(input$button, {
+      output$message <- shiny::renderText(paste("The number is", shiny::isolate(input$N)))
+    })
   }
 
   shiny::shinyApp(ui, server)
