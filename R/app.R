f2caff01e8e10bc950c84bf056acd51ad801a90b
Peter Winskill <p.winskill@imperial.ac.uk>
2023-02-08 12:40:40 +0000

    Slection update

--- R/app.R
+++ R/app.R
@@ -7,16 +7,49 @@
 app <- function(){
 
   ui <- shiny::fluidPage(
-    shiny::numericInput("N", label = "N", value = 0),
-    shiny::textOutput("message"),
-    shiny::actionButton("button","Update message")
+    shiny::fluidRow(
+      shiny::column(
+        3,
+        shiny::h4("Outcome"),
+        shiny::actionButton("button","Optimise!"),
+        shiny::textOutput("message"),
+        shiny::plotOutput("Total_plot")
+      )
+    ),
+    shiny::fluidRow(
+      # Tabs for each intervention
+      shiny::column(
+        3,
+        shiny::h4("Tabs"),
+        shiny::tabsetPanel(
+          shiny::tabPanel("N1", mapUI("N1")),
+          shiny::tabPanel("N2", mapUI("N2"))
+        )
+      )
+    )
   )
 
   server <- function(input, output, session) {
 
+    # A trigger to flag a new optimisation
+    rv <- shiny::reactiveValues(
+      selection = list(N1 = 0, N2 = 0)
+    )
+
     shiny::observeEvent(input$button, {
-      output$message <- shiny::renderText(paste("The number is", shiny::isolate(input$N)))
+      # On button click we run optimisation and update current selection
+      rv$selection$N1 <- 101
+      rv$selection$N2 <- 102
     })
+
+    # Named tab elements can use and modify reactive values
+    mapServer("N1", rv)
+    mapServer("N2", rv)
+
+    # Print current selection
+    output$message <- shiny::renderText(paste0("The selections are ", rv$selection$N1, ", ", rv$selection$N2))
+    # Plot current total
+    output$Total_plot <- shiny::renderPlot(plot(sum(unlist(rv$selection))))
   }
 
   shiny::shinyApp(ui, server)
--- R/map_module.R
+++ R/map_module.R
@@ -2,17 +2,9 @@
 #'
 #' @param id Intervention ID
 mapUI <- function(id){
-  shiny::tagList(
-    shiny::fluidRow(
-      # Map of selected spatial units
-      leaflet::leafletOutput(shiny::NS(id, "map"))
-    ),
-    shiny::fluidRow(
-      # Button to select all spatial units for given intervention
-      shiny::actionButton(shiny::NS(id, "select_all"), paste0("Select all ", id)),
-      # Button to select currently targeted spatial units for given intervention
-      shiny::actionButton(shiny::NS(id, "select_current"), paste0("Select current ", id))
-    )
+  tagList(
+    shiny::actionButton(shiny::NS(id, "mapbutton"),paste0("Clear ", id)),
+    shiny::plotOutput(shiny::NS(id, "impact_plot"))
   )
 }
 
@@ -23,66 +15,13 @@ mapUI <- function(id){
 #' @param trigger Trigger for map update
 #' @param all All subunits
 #' @param current Current subunits
-mapServer <- function(id, overwrite, trigger, all, current){
-
+mapServer <- function(id, rv){
   shiny::moduleServer(id, function(input, output, session){
-    rv <- shiny::reactiveValues()
-    rv$selection <- NULL
-    rv$clicked <- NULL
-    rv$colour <- NULL
-    rv$to_overwrite <- NULL
-    rv$overwrite_trigger <- 0
-
-    # Plot the base map
-    output$map <- base_map(mwi)
-    shiny::outputOptions(output, "map", suspendWhenHidden = FALSE)
-
-    # Selecting or deselecting a clicked polygon
-    shiny::observeEvent(input$map_shape_click, {
-      rv$clicked <- input$map_shape_click$id
-      if(rv$clicked %in% rv$selection){
-        rv$selection <- setdiff(rv$selection, rv$clicked)
-        rv$colour <- "black"
-      } else {
-        rv$selection <- c(rv$selection, rv$clicked)
-        rv$colour <- "deeppink"
-      }
-    })
-
-    # Select optimum (from app)
-    shiny::observeEvent(trigger(), ignoreInit = TRUE,  {
-      rv$to_overwrite <- overwrite()[[id]]
-      rv$overwrite_trigger <- rv$overwrite_trigger + 1
-    })
-    # Select all
-    shiny::observeEvent(input$select_all, {
-      rv$to_overwrite <- all
-      rv$overwrite_trigger <- rv$overwrite_trigger + 1
-    })
-    # Select current
-    shiny::observeEvent(input$select_current, {
-      rv$to_overwrite <- current
-      rv$overwrite_trigger <- rv$overwrite_trigger + 1
-    })
-
-    # Overwrite event
-    shiny::observeEvent(rv$overwrite_trigger, {
-      # Clear anything not in the new selection
-      to_clear <- dplyr::filter(mwi, .data$NAME_1 %in% setdiff(rv$selection, rv$to_overwrite))
-      overlap_map(leaflet::leafletProxy("map"), data = to_clear, colour = "black")
-      # Highlight anything missing from new selection
-      rv$clicked <- setdiff(rv$to_overwrite, rv$selection)
-      rv$colour <- "deeppink"
-      # Update new selection
-      rv$selection <- rv$to_overwrite
-    })
-    # Update new highlighted polygons
-    map <- shiny::reactive({
-      dplyr::filter(mwi, .data$NAME_1 %in% rv$clicked)
-    })
     shiny::observe({
-      overlap_map(leaflet::leafletProxy("map"), data = map(), colour = rv$colour)
+      output$impact_plot <- shiny::renderPlot(plot(rv$selection[[id]]))
     })
-    return(shiny::reactive(rv$selection))
+    shiny::observeEvent(input$mapbutton,{
+      rv$selection[[id]] <- 0
+    })
   })
 }
